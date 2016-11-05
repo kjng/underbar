89d697ece9ca4b6e6645eb4e544cc75cd6fd4c90
Kevin Jang <pianistkevinjang@gmail.com>
2016-11-05 03:55:51 -0400

    Implement 'map'

--- src/underbar.js
+++ src/underbar.js
@@ -128,6 +128,13 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var acc = [];
+
+    _.each(collection, function(element) {
+      acc.push(iterator(element));
+    })
+
+    return acc;
   };
 
   /*
@@ -136,7 +143,7 @@
    * as an example of this.
    */
 
-  // Takes an array of objects and returns and array of the values of
+  // Takes an array of objects and returns an array of the values of
   // a certain property in it. E.g. take an array of people and return
   // an array of just their ages
   _.pluck = function(collection, key) {
